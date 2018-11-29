cfccc6b871b239808d05c316b89f277881a5ce07
YuriiTrofimenko <tyaa@ukr.net>
2018-11-29 13:11:34 +0200

    init

---
+++ MaxNumbers/Module1.vb
@@ -0,0 +1,41 @@
+﻿Module Module1
+
+    Sub Main()
+        'Массив для хранения чисел
+        Dim nums As New List(Of Integer)()
+        Dim i As Integer = 0
+
+        Console.WriteLine("Введите последовательно не менее трех чисел. Чтобы завершить - введите 0")
+
+        Do
+            Console.Write("{0}-е число: ", i + 1)
+            nums.Add(Int32.Parse(Console.ReadLine()))
+            i = i + 1
+        Loop While nums(i - 1) <> 0
+
+        'Алгоритм сортировки
+        Dim temp As Integer = nums(0)
+        For i = 0 To nums.Count - 1
+            For j As Integer = i + 1 To nums.Count - 1
+                If nums(i) < nums(j) Then
+                    temp = nums(i)
+                    nums(i) = nums(j)
+                    nums(j) = temp
+                End If
+            Next
+        Next
+
+        'Выводим элементы массива
+        i = 0
+        For Each number As Integer In nums
+            If i = 3 Then
+                Exit For
+            End If
+            Console.Write("{0} ", number)
+            i = i + 1
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ MaxNumbers/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
+' общие сведения об этой сборке.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("MaxNumbers")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MaxNumbers")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("609842a7-31bf-4163-a99f-0dfebf5aed40")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+' Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+' используя "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
