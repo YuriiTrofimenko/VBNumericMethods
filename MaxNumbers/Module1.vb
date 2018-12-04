43361260e83297c46ea80632ee2a66181a7d93d8
YuriiTrofimenko <tyaa@ukr.net>
2018-12-04 12:29:07 +0200

    improvements

--- MaxNumbers/Module1.vb
+++ MaxNumbers/Module1.vb
@@ -1,19 +1,36 @@
 ﻿Module Module1
-
+    ''' <summary>
+    ''' Вычиследние трех наибольших чисел из введенной последовательности
+    ''' </summary>
+    ''' главная процедура
     Sub Main()
-        'Массив для хранения чисел
+        'список для хранения чисел
         Dim nums As New List(Of Integer)()
+        'число элементов в списке
         Dim i As Integer = 0
-
         Console.WriteLine("Введите последовательно не менее трех чисел. Чтобы завершить - введите 0")
-
+        'ввод чисел из консоли, пока не будет введен 0
         Do
             Console.Write("{0}-е число: ", i + 1)
-            nums.Add(Int32.Parse(Console.ReadLine()))
-            i = i + 1
-        Loop While nums(i - 1) <> 0
-
-        'Алгоритм сортировки
+            Dim newNumber = 0
+            'если введенная пользователем строка успешно приведена к целому типу,
+            'сохраняем это число в переменную newNumber и выполняем действия с ним
+            If Integer.TryParse(Console.ReadLine(), newNumber) Then
+                'если число не равно нулю - добавляем его в список
+                'и увеличиваем индекс на единицу 
+                If newNumber <> 0 Then
+                    nums.Add(newNumber)
+                    i = i + 1
+                Else
+                    'иначе, если ранее уже введено не менее трех элементов списка,
+                    'завершаем выполнение цикла 
+                    If nums.Count >= 3 Then
+                        Exit Do
+                    End If
+                End If
+            End If
+        Loop While True
+        'сортировка списка методом пузырька
         Dim temp As Integer = nums(0)
         For i = 0 To nums.Count - 1
             For j As Integer = i + 1 To nums.Count - 1
@@ -24,8 +41,8 @@
                 End If
             Next
         Next
-
-        'Выводим элементы массива
+        'вывод первых трех элементов отсортированного списка,
+        'то есть трех наибольших чисел
         i = 0
         For Each number As Integer In nums
             If i = 3 Then
@@ -34,8 +51,6 @@
             Console.Write("{0} ", number)
             i = i + 1
         Next
-
         Console.ReadLine()
     End Sub
-
 End Module
--- NewtonMethod/Module1.vb
+++ NewtonMethod/Module1.vb
@@ -1,36 +1,45 @@
 ﻿Module Module1
-
+    ''' <summary>
+    ''' Уточнение до ε=0,001 корня уравленния x^3+3*x^2-3,
+    ''' расположенного на отрезке  [-2,75; -2,5],
+    ''' методом Ньютона (касательных)
+    ''' </summary>
+    ''' главная процедура
     Sub Main()
-        Dim result = SearchRootNewton(x1:=-2.75, x2:=-2.5, e:=0.0001)
+        'вызов функции уточнения корня
+        Dim result = SearchRootNewton(a:=-2.75, b:=-2.5, e:=0.001)
+        'вывод результата
         Console.WriteLine(result)
-        Console.WriteLine(F(result))
+        'вывод проверки -
+        'при подставновке ответа в функцию получается приблизительно 0
+        'Console.WriteLine(F(result))
     End Sub
-
+    'функция, для которой уточняется корень
     Function F(x) As Double
         F = x ^ 3 + 3D * x ^ 2 - 3D
-        'Console.WriteLine(F)
     End Function
-
-    Function dFdX(x As Double) As Double
+    'функция, вычисляющая значение производной исходной функции
+    Function dFdx(x As Double) As Double
         Dim h As Double
-        h = 0.0000001
-        dFdX = (F(x + h) - F(x - h)) / (2 * h)
-        'dFdX = (F(x + h) - F(x)) / h
+        h = 0.0001
+        dFdx = (F(x + h) - F(x - h)) / (2 * h)
     End Function
-
-    Function SearchRootNewton(ByRef x1 As Double, ByRef x2 As Double, ByRef e As Double) As Double
-        Dim Result As Double
-        Dim x As Double
-        Dim a As Double
-        x = x1
-        a = F(x) / dFdX(x)
-        Do While (Math.Abs(a) > e) And (x < x2)
-            x = x - a
-            a = F(x) / dFdX(x)
-            'Console.WriteLine(x)
+    'функция уточнения
+    Function SearchRootNewton(ByRef a As Double, ByRef b As Double, ByRef e As Double) As Double
+        Dim xa = a
+        Dim xb = b
+        'повторение цикла, пока не достигнута нужная точность результата
+        Do While (Math.Abs(xb) > e)
+            'вычисление корня по формуле
+            xa -= F(xa) / dFdx(xa)
+            'вычисление полученной точности
+            xb = F(xa)
+            'вывод промежуточных результатов
+            'Console.WriteLine($"xa = {xa}")
+            'Console.WriteLine($"xb = {xb}")
         Loop
-        Result = x
-        SearchRootNewton = Result
+        'возврат уточненного значения функцией
+        SearchRootNewton = xa
     End Function
 
 End Module
